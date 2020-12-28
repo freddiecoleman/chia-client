1d5a6286fa557192fb5baca6b2a0a22cf1c7b31e
Freddie Coleman <f.coleman@hotmail.co.uk>
2020-12-28 21:39:24 +0000

    remove hashHeader from old blockchain format

--- index.ts
+++ index.ts
@@ -1,9 +1,7 @@
-import { hashHeader } from './src/hashHeader';
 import { generatePlotId } from './src/generatePlotId';
 import { FullNode } from './src/FullNode';
 
 export {
     FullNode,
-    hashHeader,
     generatePlotId
 };
