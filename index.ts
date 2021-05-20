f048b21c2c5ea5885fd51ba878148a40a2a20d95
MadddinTribleD <martin@gafert.org>
2021-05-20 13:13:57 +0200

    Add RPC calls for Harvester

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import { FullNode } from './src/FullNode';
 import { Wallet } from './src/Wallet';
+import { Harvester } from './src/Harvester';
 import { SharedCalls } from './src/SharedCalls'
 
 export {
     FullNode,
+    Harvester,
     Wallet,
     SharedCalls,
 };
---
+++ src/types/Harvester/Plot.ts
@@ -0,0 +1,10 @@
+export interface Plot {
+    file_size: number;
+    filename: string;
+    'plot-seed': string;
+    plot_public_key: string;
+    pool_contract_puzzle_hash: string;
+    pool_public_key: string;
+    size: number;
+    time_modified: number;
+}
