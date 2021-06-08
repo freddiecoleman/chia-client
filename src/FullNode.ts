1ed829f2285e3d10a4acc5fc572397f0d5936e21
Kabir R <contact@kabirwearsato.ga>
2021-06-08 09:08:22 -0700

    Update FullNode.ts

--- src/FullNode.ts
+++ src/FullNode.ts
@@ -14,7 +14,7 @@ import { Block } from "./types/FullNode/Block";
 import { CertPath } from "./types/CertPath";
 import { getChiaConfig, getChiaFilePath } from "./ChiaNodeUtils";
 // @ts-ignore
-import { address_to_puzzle_hash, puzzle_hash_to_address, get_coin_info } from "chia-utils";
+import { address_to_puzzle_hash, puzzle_hash_to_address, get_coin_info_mojo } from "chia-utils";
 
 const chiaConfig = getChiaConfig();
 const defaultProtocol = "https";
